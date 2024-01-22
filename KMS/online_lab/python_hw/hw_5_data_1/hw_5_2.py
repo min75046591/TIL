c24d1e7359fb577cbd7370fe3c8a2500deb491e0
Min <min73056591@gamil.com>
2024-01-23 00:39:30 +0900

    done

--- KMS/online_lab/python_hw/hw_5_data_1/hw_5_2.py
+++ KMS/online_lab/python_hw/hw_5_data_1/hw_5_2.py
@@ -1,8 +1,19 @@
 # 아래 함수를 수정하시오.
-def count_character(text, char_to_count):
+def count_character(text, char_to_count): # 두개의 매개변수
+    # text : 문자를 세고자 하는 대상이 되는 텍스트 ('Hello, World')
+    # char_to_count : 세고자 하는 특정 문자 ('o')
     result = text.count(char_to_count)
     return result
 
 # 사용 예시:
 result = count_character("Hello, World!", "o")
 print(result)  # 출력: 2
+
+
+# def count_character(char_to_count):
+#     result.count(char_to_count)
+#     return result
+
+# result = count_character("Hello, World!", "o")
+
+# print(result)
--- KMS/online_lab/python_hw/hw_5_data_1/hw_5_4.py
+++ KMS/online_lab/python_hw/hw_5_data_1/hw_5_4.py
@@ -11,10 +11,10 @@ num_list = [3, 1, 7, 2, 5]
 # print(result)  # (1, 7)
 
 
-max_val = num_list[0]
+max_val = num_list[0] # 변수를 리스트의 첫 번째 원소로 초기화
 min_val = num_list[0]
 
-for num in num_list:
+for num in num_list:  # 반복문을 통한 최솟값과 최대값 찾기
     if num > max_val:
         max_val = num
     elif num < min_val:
