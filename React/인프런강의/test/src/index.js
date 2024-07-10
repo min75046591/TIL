a35e01d9adcd05e1a315cba4bd3791bbaca215b1
Min <min75046591@gmail.com>
2024-07-10 23:33:06 +0900

    3강 완료

--- React/인프런강의/test/src/index.js
+++ React/인프런강의/test/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+import Library from './chapter_03/Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
